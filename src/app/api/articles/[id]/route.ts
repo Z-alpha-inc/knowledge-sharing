85ff609258647a11d970c7ab48564fd00287c001
Eztas <nietzsche2669sensei@gmail.com>
2025-10-17 20:57:31 +0900

    add: DELETEメソッドの追加

--- src/app/api/articles/[id]/route.ts
+++ src/app/api/articles/[id]/route.ts
@@ -2,9 +2,10 @@
 import { prisma } from '@/lib/prisma';
 import { NextResponse, NextRequest } from 'next/server';
 
-// 記事一覧を取得するAPI, GET /api/articles/[id]を構築
+// 記事1つだけを取得するAPI, GET /api/articles/[id]を構築
 // .httpメソッドでデータも取得できる
 export async function GET(
+    request: NextRequest, // 使うわけではないがこれがないと、URLパラメータが取得できない
     { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
@@ -27,7 +28,38 @@ export async function GET(
     return NextResponse.json(article, { status: 200 });
   }catch (error) {
     console.error(error);
-    return NextResponse.json({ error: 'Failed to fetch articles' }, { status: 500 });
+    return NextResponse.json({ error: 'Failed to fetch an article' }, { status: 500 });
+  }
+
+}
+
+// 記事1つを削除するAPI, DELETE /api/articles/[id]を構築
+// .httpメソッドでデータも削除できる
+export async function DELETE(
+    request: NextRequest, // 使うわけではないがこれがないと、URLパラメータが取得できない
+    { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
+  // prismaを使うことで、prisma.article.delete()でarticlesテーブルの全データを取得
+  try{
+    const article = await prisma.article.delete({
+        where:{
+            id: id // ここに削除したい記事のIDを指定
+        }
+        //include: {
+        //  author: true,
+        //},
+    });
+
+    if (!article) {
+      return NextResponse.json({ error: 'Article not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(article, { status: 200 });
+  }catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Failed to delete an article' }, { status: 500 });
   }
 
 }

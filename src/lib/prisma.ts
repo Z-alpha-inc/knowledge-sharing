d14cf170e3cd39167ef70ad98f98732d45c5df62
Eztas <nietzsche2669sensei@gmail.com>
2025-10-19 08:06:45 +0900

    fix: wip: Prisma Clientが複数生成されることを防止するための公式対策(シングルトンパターン)を追加

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,17 @@
+// src/lib/prisma.ts
 // Prisma Clientをインポートする
 import { PrismaClient } from '@/generated/prisma/client';
 
-// インスタンスを作成する (これがDB操作の窓口になる)
-// データベースへのすべての操作（データの取得、作成、更新、削除など）を実行するためのインスタンス
-export const prisma = new PrismaClient();
+// https://www.prisma.io/docs/orm/prisma-client/setup-and-configuration/databases-connections
+// Next.jsのホットリロード時などにPrisma Clientのインスタンスが複数作成されるのを防ぐための設定
+// これでデータベース接続がエラーになった時に修正はできなかったが
+// 未然に防げる「可能性」はあるので、一度実装
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+export const prisma = 
+  globalForPrisma.prisma ||  // 既にあれば再利用
+  new PrismaClient()         // なければ新規作成
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma  // グローバルに保存
+}

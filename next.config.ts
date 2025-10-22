99cd4f90778ec5508e9850c18003fe46790f86d2
Eztas <nietzsche2669sensei@gmail.com>
2025-10-22 16:15:36 +0900

    change: ローカルホストのトップページを全社向けに変更

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/', // サイトのトップページを指す
+        destination: '/spaces/all', // トップページに来たときの転送先パス(リダイレクト先)
+        permanent: true, // このURLは恒久的に /spaces/all に移動することを示す (SEOに強い, 検索エンジンもこっちを優先して表示)
+      },
+    ]
+  },
 };
 
 export default nextConfig;
